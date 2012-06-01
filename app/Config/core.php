df7a6f6b7fcac1097ea69d3a57b8b6359d879d07
Rob Sawyer <robksawyer@gmail.com>
2012-06-01 00:34:20 -0700

    Just cranking it up

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'ASDFaskjakljfas89789asjkans;[---asdf-as-dfasdf00]');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '9879834598347589345938753');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
---
+++ app/View/Themed/Version_1/Pages/home.ctp
@@ -0,0 +1,3 @@
+<div class="landing-page">
+	<p>n-b-d-b is a new way to discover art you'll love, featuring work found in restaurants that barely hangs straight, modern art from specialty shops, stained art at coffee shops and private collections around the world.</p>
+</div>
